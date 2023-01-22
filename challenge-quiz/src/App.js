f5fa03aa6ee1478892f8deffd80ba55b53235d24
Mariam-mohamed110 <mariammohamed0313@gmail.com>
2023-01-22 16:12:15 +0000

    finished quiz

--- challenge-quiz/src/App.js
+++ challenge-quiz/src/App.js
@@ -7,11 +7,60 @@ import Question from "./components/Question";
 function App() {
   const [lives, setLives] = useState(3);
   const [score, setScore] = useState(0);
+  const [question, setQuestion] = useState("");
+  const [answers, setAnswers] = useState([]);
+  const [correctAnswer, setCorrectAnswer] = useState("");
+
+  useEffect(() => {
+    const getQuestion = async () => {
+      const res = await fetch(
+        "https://opentdb.com/api.php?amount=1&difficulty=easy&encode=url3986"
+      );
+      const json = await res.json();
+      const decodedQuestion = decodeURIComponent(
+        json["results"][0]["question"]
+      );
+
+      const decodedCorrectAnswer = decodeURIComponent(
+        json["results"][0]["correct_answer"]
+      );
+      const decodedIncorrectAnswer = decodeURIComponent(
+        json["results"][0]["incorrect_answers"]
+      );
+      const decodedAnswers = [decodedCorrectAnswer, decodedIncorrectAnswer]
+        .toString()
+        .split(",");
+
+      const shuffleAnswers = decodedAnswers.sort(() => Math.random() - 0.5);
+
+      setQuestion(decodedQuestion);
+      setAnswers(shuffleAnswers);
+      setCorrectAnswer(decodedCorrectAnswer);
+    };
+    getQuestion();
+  }, [score, lives]);
+
+  if (lives === 0) {
+    alert(`You lost!\nYour score is ${score}`);
+    window.location.reload(true);
+  }
+
+  const checkAnswer = (e) => {
+    if (e.target.textContent === correctAnswer) {
+      return setScore(score + 1);
+    } else {
+      return setLives(lives - 1);
+    }
+  };
 
   return (
     <div className="App">
       <LiveScore lives={lives} score={score} />
-      <Question />
+      <Question
+        answers={answers}
+        checkAnswer={checkAnswer}
+        question={question}
+      />
     </div>
   );
 }
--- challenge-quiz/src/components/Question.jsx
+++ challenge-quiz/src/components/Question.jsx
@@ -1,39 +1,53 @@
-import { React, useState, useEffect } from "react";
+import { React } from "react";
 
-export default function Question() {
-  const [question, setQuestion] = useState("");
-  const [answers, setAnswers] = useState([]);
+export default function Question({ answers, checkAnswer, question }) {
+  //   const [question, setQuestion] = useState("");
+  //   const [answers, setAnswers] = useState([]);
+  //   const [correctAnswer, setCorrectAnswer] = useState("");
 
-  useEffect(() => {
-    const getQuestion = async () => {
-      const res = await fetch(
-        "https://opentdb.com/api.php?amount=1&difficulty=easy&encode=url3986"
-      );
-      const json = await res.json();
-      const decodedQuestion = decodeURIComponent(
-        json["results"][0]["question"]
-      );
+  //   useEffect(() => {
+  //     const getQuestion = async () => {
+  //       const res = await fetch(
+  //         "https://opentdb.com/api.php?amount=1&difficulty=easy&encode=url3986"
+  //       );
+  //       const json = await res.json();
+  //       const decodedQuestion = decodeURIComponent(
+  //         json["results"][0]["question"]
+  //       );
 
-      const decodedCorrectAnswer = decodeURIComponent(
-        json["results"][0]["correct_answer"]
-      );
-      const decodedIncorrectAnswer = decodeURIComponent(
-        json["results"][0]["incorrect_answers"]
-      );
-      const decodedAnswers = [decodedCorrectAnswer, decodedIncorrectAnswer]
-        .toString()
-        .split(",");
+  //       const decodedCorrectAnswer = decodeURIComponent(
+  //         json["results"][0]["correct_answer"]
+  //       );
+  //       const decodedIncorrectAnswer = decodeURIComponent(
+  //         json["results"][0]["incorrect_answers"]
+  //       );
+  //       const decodedAnswers = [decodedCorrectAnswer, decodedIncorrectAnswer]
+  //         .toString()
+  //         .split(",");
 
-      const shuffleAnswers = decodedAnswers.sort(() => Math.random() - 0.5);
+  //       const shuffleAnswers = decodedAnswers.sort(() => Math.random() - 0.5);
 
-      setQuestion(decodedQuestion);
-      setAnswers(shuffleAnswers);
-    };
-    getQuestion();
-  }, []);
+  //       setQuestion(decodedQuestion);
+  //       setAnswers(shuffleAnswers);
+  //       setCorrectAnswer(decodedCorrectAnswer);
+  //     };
+  //     getQuestion();
+  //   }, []);
 
-  const buttons = answers.map((answer) => {
-    return <button>{answer}</button>;
+  //   const checkAnswer = (e) => {
+  //     if (e.target.textContent === correctAnswer) {
+  //       return score++;
+  //     } else {
+  //       return lives++;
+  //     }
+  //   };
+
+  const buttons = answers.map((answer, index) => {
+    return (
+      <button key={index} onClick={(e) => checkAnswer(e)}>
+        {answer}
+      </button>
+    );
   });
 
   return (
